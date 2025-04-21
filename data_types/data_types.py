4465bd83cd66f8034e4d426fe2de97c5050259ca
Boris Petrov <borpetro@amazon.com>
2025-04-21 15:34:15 +0300

    refactoring

--- data_types/data_types.py
+++ data_types/data_types.py
@@ -1,3 +1,6 @@
+# Data Types in Python
+# Variables are used to store data that can be used later in the program.
+
 name = "boris petrov"
 print(name.title())
 
@@ -7,12 +10,17 @@ last_name = "Petrov"
 print(f"hello {first_name} {last_name} good luck!")
 
 
+# String are immutable, meaning they cannot be changed.
+# To change a string, you must create a new string with the desired changes.
+
+name = "boris petrov"
 favroite_language = "python "
 print(favroite_language.rstrip())
-
+# String methods
 other_language = '   java'   
 print(other_language.lstrip())
 
+# String concatenation
 quote_person = "Albert Einstein"
 quote_message = "A person who never made a mistake never tried anything new."
 print(f"{quote_person} once said, '{quote_message}'")
