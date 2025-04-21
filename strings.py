9ed632c906e53e5c5fddbf52b39c92d78928c63f
Boris Petrov <borpetro@amazon.com>
2025-04-21 13:25:54 +0300

    getting started

---
+++ strings.py
@@ -0,0 +1,18 @@
+name = "boris petrov"
+print(name.title())
+
+
+first_name = "Boris"
+last_name = "Petrov"
+print(f"hello {first_name} {last_name} good luck!")
+
+
+favroite_language = "python "
+print(favroite_language.rstrip())
+
+other_language = '   java'   
+print(other_language.lstrip())
+
+
+quote = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+print(quote)    
