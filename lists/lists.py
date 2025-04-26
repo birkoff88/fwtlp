ffb84d57d64b0514e7364c814ff7f3172dca518e
Boris Petrov <borpetro@amazon.com>
2025-04-26 19:19:05 +0300

    work with list

--- lists/lists.py
+++ lists/lists.py
@@ -1,2 +1,13 @@
-bicycle = ['trek', 'specialized', 'cannondale', 'giant']
-print(bicycle[0])
+bicycles = ['trek', 'specialized', 'canyon', 'giant']
+
+message = f"My first bicycle was a {bicycles[0].title()}."
+
+print(message)
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki'] 
+print(motorcycles[1])
+motorcycles.append('ducati')
+print(motorcycles)
+motorcycles.insert(0, 'harley')
+print(motorcycles)
