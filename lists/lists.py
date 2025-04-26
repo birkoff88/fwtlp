e38c99ebfcfb57388568ae665881c9f26b96dbd6
Boris Petrov <borpetro@amazon.com>
2025-04-26 23:57:25 +0300

    work with list

--- lists/lists.py
+++ lists/lists.py
@@ -8,6 +8,22 @@ print(message)
 motorcycles = ['honda', 'yamaha', 'suzuki'] 
 print(motorcycles[1])
 motorcycles.append('ducati')
-print(motorcycles)
+print(f"added motorcycle: {motorcycles[3]}", motorcycles)
 motorcycles.insert(0, 'harley')
+print(motorcycles)
+motorcycles.insert(3, 'bmw')
+# insert at index 3
+print(motorcycles)
+del motorcycles[3]
+# delete at index 3
+print(motorcycles)  
+
+poped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(poped_motorcycle)
+
+
+
+expensive_motorcycles = 'honda'
+motorcycles.remove(expensive_motorcycles)
 print(motorcycles)
